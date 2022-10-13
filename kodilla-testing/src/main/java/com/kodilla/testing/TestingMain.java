31e17254da39f43e28da8678399565dbe71fb07e
Jakub Stecko <jakub.stec01@gmail.com>
2022-10-13 16:39:18 +0200

    6.2 Calculator testing

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,24 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+
 public class TestingMain {
+
     public static void main(String[] args) {
-        System.out.println("Moduł 6.Wprowadzenie do testowania oporgramowania");
+        Calculator calculator = new Calculator();
+
+        int result1 = calculator.add(6,5);
+        int result2 = calculator.substract(6,5);
+
+        if (result1 == 11) {
+            System.out.println("test1 OK");
+        } else {
+            System.out.println("Error!");
+        }
+        if (result2 == 1){
+            System.out.println("test2 OK");
+        }else{
+            System.out.println("Error");
+        }
     }
 }
