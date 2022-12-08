41f633a056b155acea39eb78a7089fac7ccabdad
Jakub Stecko <jakub.stec01@gmail.com>
2022-12-08 20:10:21 +0100

    7.2 Niemutowalny uzytkownik forum

--- kodilla-stream/src/main/java/com/kodilla/stream/beautifier/PoemBeautifier.java
+++
@@ -1,8 +0,0 @@
-package com.kodilla.stream.beautifier;
-
-public class PoemBeautifier {
-    public void beautify(String something, PoemDecorator poemDecorator){
-        String someText = poemDecorator.decorate(something);
-        System.out.println("Text beautify words : " + someText);
-    }
-}
