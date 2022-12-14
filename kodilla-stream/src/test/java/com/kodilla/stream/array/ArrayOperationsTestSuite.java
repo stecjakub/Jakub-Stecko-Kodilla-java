6e78d458d0b918c426031f21e81aa6c9d9135d8f
Jakub Stecko <jakub.stec01@gmail.com>
2022-12-14 22:33:31 +0100

    7.5 Average and IntStream

--- kodilla-stream/src/test/java/com/kodilla/stream/array/ArrayOperationsTestSuite.java
+++ kodilla-stream/src/test/java/com/kodilla/stream/array/ArrayOperationsTestSuite.java
@@ -1,2 +1,17 @@
-package com.kodilla.stream.array;public class ArrayOperationsTestSuite {
+package com.kodilla.stream.array;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.stream.IntStream;
+
+public class ArrayOperationsTestSuite {
+    @Test
+    void testGetAverage(){
+        int[] numbers = IntStream.range(4,16).toArray();
+        double average = ArrayOperations.getAverage(numbers);
+        //System.out.println(average);
+        Assertions.assertEquals(9.5,average);
+
+    }
 }
