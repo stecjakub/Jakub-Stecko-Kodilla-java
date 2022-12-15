9efbe347f606b56b6d903db12db3f82d735876c7
Jakub Stecko <jakub.stec01@gmail.com>
2022-12-15 22:31:52 +0100

    6.3 OddNumbers Test

---
+++ kodilla-testing/src/main/java/com/kodilla/testing/collection/OddNumbersExterminator.java
@@ -0,0 +1,16 @@
+package com.kodilla.testing.collection;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class OddNumbersExterminator {
+    public List<Integer> exterminate(List<Integer> numbers) {
+        List<Integer> evenNumbers = new LinkedList<>();
+        for(int number : numbers) {
+            if(number % 2 ==0) {
+                evenNumbers.add(number);
+            }
+        }
+        return evenNumbers;
+    }
+}
