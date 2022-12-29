b527752800fc58b043a8772286872d716d189dae
Jakub Stecko <jakub.stec01@gmail.com>
2022-12-29 19:17:39 +0100

    8.2 First Challenge

--- kodilla-exception/src/main/java/com/kodilla/exception/test/FirstChallenge.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/FirstChallenge.java
@@ -9,14 +9,17 @@ public class FirstChallenge {
         return a / b;
     }
 
-    /**
-     * This main can throw an ArithmeticException!!!
-     * @param args
-     */
     public static void main(String[] args) {
         FirstChallenge firstChallenge = new FirstChallenge();
-        double result = firstChallenge.divide(3, 0);
+        try {
+            double result = firstChallenge.divide(3, 0);
+            System.out.println(result);
+        }catch(ArithmeticException e){
+            System.out.println("Divide by 0" + e);
+        }
+        finally{
+            System.out.println("Program finished" );
 
-        System.out.println(result);
+        }
     }
 }
