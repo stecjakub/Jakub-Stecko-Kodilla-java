745209c8cb3d2ef7f144aaec4e260e6ab250492b
Jakub Stecko <jakub.stec01@gmail.com>
2023-02-06 16:01:23 +0100

    14.4 Spring web

--- kodilla-spring-web/src/main/java/com/kodilla/kodillaspringweb/KodillaSpringWebApplication.java
+++ kodilla-spring-web/src/main/java/com/kodilla/kodillaspringweb/KodillaSpringWebApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class KodillaSpringWebApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(KodillaSpringWebApplication.class, args);
-	}
-
+    public static void main(String[] args){
+        SpringApplication.run(KodillaSpringWebApplication.class, args);
+    }
 }
