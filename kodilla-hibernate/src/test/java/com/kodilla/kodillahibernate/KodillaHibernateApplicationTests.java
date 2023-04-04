c4174139239a7246d533dc64bbb5fedabe946632
Jakub Stecko <jakub.stec01@gmail.com>
2023-04-04 22:26:52 +0200

    17 hibernate

---
+++ kodilla-hibernate/src/test/java/com/kodilla/kodillahibernate/KodillaHibernateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kodilla.kodillahibernate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KodillaHibernateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
